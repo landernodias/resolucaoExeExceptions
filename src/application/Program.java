afea8304e8ea900ecb13636c92a4ac42f1aaadd2
Nelcione Dias <nelcionedias@gmail.com>
2022-05-16 12:10:26 -0300

    vary bad soluction

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,46 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+
 public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter account data:");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance:");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw Limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		if(amount > acc.getWithdrawLimit()) {
+			System.out.println("Withdraw error: The amount exceeds withdraw limit");
+		} else if(amount > acc.getBalance()){
+			System.out.println("Withdraw error: Not enough balance");
+		}else {
+			acc.withdraw(amount);
+			System.out.printf("Novo saldo: %.2f", acc.getBalance());			
+		}
+		
+		
+		
+		sc.close();
+	}
 
 }
